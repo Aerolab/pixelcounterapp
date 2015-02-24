c56c81c1e960a52f410e85558e0f426435a61e8d
Israel Wegierski <israelmw@hotmail.com>
2015-02-24 18:56:33 -0300

    middle implementation with bugs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,7 +4,7 @@ var availableFormats = ['bmp', 'gif', 'jpg', 'jpeg', 'png', 'psd', 'tiff', 'webp
 
 // Drag and Drop
 var dropArea = document.getElementById('dropArea');
-var titleFolder = $('.onFolderin .title');
+var titleFolder = $('.onFolderin .title, .finish .folder .name');
 var quantityFolder = $('.onFolderin .quantity');
 var quantityNumberFolder = $('.onFolderin .quantity .number');
 var dragged;
@@ -59,32 +59,130 @@ dropArea.ondrop = function(event) {
     // Add paths folder
     pathsFolders.push(item.path);
   }
-
   // Mostrar el nombre de las carpetas
   if (namesFolders.length > 2) {
     titleFolder.text(namesFolders[0]+', etc..');
   }else{
     titleFolder.text( namesFolders.join(', ') );
   }
+  // Press Start
+  $('.start').click(function(event) {
+    event.preventDefault();
+    // if droparea has folder in.
+    if ($('#dropArea').hasClass('folderin')) {
+      initCount(pathsFolders);
+    };
+  });
 
 };
 
 
-$(document).ready(function() {
-
-  // Odometer and counter
+// Odometer and counter
   var countReduced = $('.count .reduced .number')[0];
   var countFull = $('.count .full .number');
   odometer = new Odometer({
     el: countReduced,
     value: 0,
-    format: '.ddd',
+    format: 'd',
+    duration: 1500,
     theme:'default'
   });
   odometer.render();
-  // setTimeout(odometer.update(1564),5000);
 
-  // Pixel Counter Module
-  // pixelCounter.start(directory, scanFormats, onCount, onEnd);
+// OnCount
 
+// Init Count
+var initCount = function (pathsFolders) {
+  // Show View On Count
+  $('.oncount').addClass('show');
+  // Start pixel counter
+  for (dir in pathsFolders) {
+    console.log('arranco con la carpeta: '+pathsFolders[dir]);
+    pixelcounter.start(pathsFolders[dir], availableFormats, onCount, onEnd);
+  }
+}
+var interval;
+// Show Progress
+var pixelsProgress;
+var onCount = function (path, filePixelsArea, pixels) {
+  pixelsProgress = pixels;
+  // Update full
+  $('.count .full .number').text(pixelsProgress);
+}
+interval = setInterval(function () {
+  if (pixelsProgress) {
+    console.log('pixels contados: '+pixelsProgress);
+    updateOdometer(pixelsProgress);
+  };
+}
+,3000);
+// Update Count
+var updateOdometer = function (pixelsProgress) {
+  var reduced = abbrNum(pixelsProgress);
+  var number = reduced.n;
+  var letter = reduced.l;
+  // Update odometer
+  odometer.update(number);
+  $('.count .reduced .unit').text(letter);
+
+}
+// Update Finish
+var updateFinish = function (pixelsFinish) {
+  var reduced = abbrNum(pixelsFinish);
+  var number = reduced.n;
+  var letter = reduced.l;
+  // Update odometer
+  $('.result .reduced .number').text(number);
+  $('.result .reduced .unit').text(letter);
+  $('.result .full .number').text(pixelsFinish);
+
+}
+// Abbreviations Number
+function abbrNum(number) {
+    // result Object
+    var result = new Object;
+    // Enumerate number abbreviations
+    var abbrev = [ "k", "m", "b", "t" ];
+    // Go through the array backwards, so we do the largest first
+    for (var i=abbrev.length-1; i>=0; i--) {
+        // Convert array index to "1000", "1000000", etc
+        var size = Math.pow(10,(i+1)*3);
+        // If the number is bigger or equal do the abbreviation
+        if(size+'0' <= number) {
+             result.n = parseInt(number/size);
+             // Add the letter for the abbreviation
+             result.l = abbrev[i];
+             break;
+        }else{
+          result.n = number;
+          result.l = "";
+        }
+    }
+    return result;
+}
+
+// Show Finish
+// OnEnd
+var onEnd = function (p) {
+  console.log(p);
+  console.log('termine');
+  clearInterval(interval);
+  pixelsProgress=null;
+  // Update Finish
+  updateFinish(p);
+  // Show Finish
+  $('.oncount').removeClass('show');
+  $('.finish').addClass('show');
+}
+// Start Over
+var resetFirstep = function () {
+  dropArea.className = 'dragfold';
+
+};
+
+$('.start-over').click(function(event) {
+  event.preventDefault();
+  $('.finish').removeClass('show');
+  dropArea.className = 'dragfold';
+  // $('.finish').addClass('show');
 });
