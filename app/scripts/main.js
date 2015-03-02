c431b22639f4eed95a5d9fc6e98152d11b9247d7
Israel Wegierski <israelmw@hotmail.com>
2015-03-02 15:58:51 -0300

    Flow section implementation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,13 @@
+window._is_mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
 $(function() {
+
+  if ($('#flow').length == 1) {
+
+    if( _is_mobile ) {
+      $('#flow').addClass('is_mobile');
+    } else {
+      var s = skrollr.init({forceHeight: false});
+    }
+  }
 });
