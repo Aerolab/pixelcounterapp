e019ead2e9b9b764a0bea25f705ea9d45d82c5c4
Israel Wegierski <israelmw@hotmail.com>
2015-03-10 15:47:02 -0300

    fix variable config in task linux dist

--- grunt-tasks/register/createLinuxApp.js
+++ grunt-tasks/register/createLinuxApp.js
@@ -7,6 +7,7 @@ module.exports = function (grunt) {
     var done = this.async();
     var childProcess = require('child_process');
     var exec = childProcess.exec;
+    var config = grunt.config.get(['config']);
     var path = './' + (version === 'Linux64' ? config.distLinux64 : config.distLinux32);
     exec('mkdir -p ' + path + '; cp resources/node-webkit/' + version + '/nw.pak ' + path + ' && cp resources/node-webkit/' + version + '/nw ' + path + '/node-webkit && cp resources/node-webkit/' + version + '/icudtl.dat ' + path + '/icudtl.dat', function (error, stdout, stderr) {
       var result = true;
